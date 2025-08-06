62a330c8f41c08303b0a5df79b15469beafc350d
rajmr4u <rajmr4u@users.noreply.github.com>
2025-08-06 15:53:31 +0200

    Initial Commit

---
+++ src/main/java/com/ecosphere/esg/EcosphereApplication.java
@@ -0,0 +1,13 @@
+package com.ecosphere.esg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcosphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcosphereApplication.class, args);
+	}
+
+}
